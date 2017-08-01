42a28033edb55e352657aca1f3791168fb1db691
jasonwynn10 <jasonwynn10@gmail.com>
2017-08-01 14:41:59 -0400

    Compact code and remove unused use statements Correct PhpDoc comments All spacing is not the same Version bump due to new feature and BC breaks

--- src/Ad5001/Elytra/tasks/AdminGotoTask.php
+++ src/Ad5001/Elytra/tasks/AdminGotoTask.php
@@ -1,88 +1,26 @@
 <?php
-
-
 namespace Ad5001\Elytra\tasks;
-
-
-
-use pocketmine\Server;
-
 
 use pocketmine\scheduler\PluginTask;
 
-
-use pocketmine\utils\BlockIterator;
-
-
-use pocketmine\Player;
-
-
-
 use Ad5001\Elytra\Main;
-
-
-
-
-
-
 
 class AdminGotoTask extends PluginTask {
 
-
-
-
-   public function __construct(Main $main) {
-
-
-        parent::__construct($main);
-
-
-        $this->main = $main;
-
-
-        $this->server = $main->getServer();
-
-
-    }
-
-
-
-
-   public function onRun($tick) {
-       foreach ($this->server->getOnlinePlayers() as $player) {
-
-
-           //Part needed for player's good working
-           $ref = new \ReflectionClass("pocketmine\\Player");
-           $prop = $ref->getProperty("gravity");
-           $prop->setAccessible(true);
-           $prop->setValue($player, 0);
-           $prop->setAccessible(false);
-           if($player->getMotion()->y !== 0) {
-            //    echo "{$player->getName()}:" . $player->getMotion()->y . "\n";
-           }
-       }
-    }
-
-
-
-    /*
-    Inverts a number
-    @param     $num    int
-    @return int
-    */
-    public function invert(int $num) : int {
-        if($num < 0) {
-            echo $num . " +> " . abs($num) . "\n";
-            return abs($num);
-        } else {
-            echo $num . " -> " . -$num . "\n";
-            return -$num;
-        }
-        return 0;
-    }
-
-
-
-
+	public function __construct(Main $main) {
+		parent::__construct($main);
+	}
+	public function onRun(int $tick) {
+		foreach ($this->getOwner()->getServer()->getOnlinePlayers() as $player) {
+			//Part needed for players to fly upwards
+			$ref = new \ReflectionClass("pocketmine\\Player");
+			$prop = $ref->getProperty("gravity");
+			$prop->setAccessible(true);
+			$prop->setValue($player, 0);
+			$prop->setAccessible(false);
+			if($player->getMotion()->y !== 0) {
+				$this->getOwner()->getLogger()->debug("{$player->getName()}'s y motion is {$player->getMotion()->y}");
+			}
+		}
+	}
 }
